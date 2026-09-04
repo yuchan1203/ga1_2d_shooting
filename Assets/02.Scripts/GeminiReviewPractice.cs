4b557b69aa55d84b0d91be54638f5fe35086004e
yuchan hwang <hwangyuchan06@gmail.com>
2026-09-04 10:22:49 +0900

    fix: 테스트 준비

--- Assets/02.Scripts/GeminiReviewPractice.cs
+++ Assets/02.Scripts/GeminiReviewPractice.cs
@@ -1,14 +1,1 @@
-﻿using UnityEngine;
-
-public class GeminiReviewPractice : MonoBehaviour
-{
-    private void Update()
-    {
-        GameObject player = GameObject.Find("Player");
-
-        Rigidbody2D playerRigidbody = player.GetComponent<Rigidbody2D>();
-        playerRigidbody.AddForce(Vector2.Up * 10f);
-
-        Debug.Log($"Player Position: {player.transform.position}");
-    }
-}
+﻿
