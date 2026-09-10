c3688f1593ebea36c62d01c90faaf022dc2ebcff
yuchan hwang <hwangyuchan06@gmail.com>
2026-09-10 11:05:08 +0900

    fix: 플레이어 자동 이동 개선

--- Assets/02.Scripts/Player/PlayerAutoMove.cs
+++ Assets/02.Scripts/Player/PlayerAutoMove.cs
@@ -2,16 +2,47 @@ using UnityEngine;
 public class PlayerAutoMove : MonoBehaviour
 {
     [SerializeField] private float _speed;
+    private GameObject _target = null;
+    private void FindTarget()
+    {
+        if (_target != null) return;
+        GameObject[] targets = GameObject.FindGameObjectsWithTag("Enemy");
+        if (targets.Length == 0) return;
+        _target = targets[0];
+        float minDistance = float.MaxValue;
+        foreach (GameObject enemy in targets)
+        {
+            float distance = Vector2.Distance(transform.position, enemy.transform.position);
+            if (distance < minDistance)
+            {
+                minDistance = distance;
+                _target = enemy;
+            }
+        }
+    }
+    private void Move()
+    {
+        if (_target == null)
+        {
+            return;
+        }
+        Vector3 diff = _target.transform.position - transform.position;
+        Vector3 direction = diff.normalized;
+        if (diff.y >= 3)
+        {
+            direction.y = 1;
+        }
+        else
+        {
+            direction.y = -1;
+        }
+        //diff.y = 0;
+        direction.Normalize();
+        transform.position += direction * (_speed * Time.deltaTime);
+    }
     private void Update()
     {
-        // 타겟 구하기
-        GameObject target = GameObject.FindWithTag("Enemy");
-        if (target == null) return;
-        // 방향 구하기
-        Vector3 direction = target.transform.position - transform.position;
-        direction.Normalize();
-        // 이동하기 
-        direction.y = 0;
-        transform.position += direction * _speed * Time.deltaTime;
+        FindTarget();
+        Move();
     }
 }
